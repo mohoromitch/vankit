c7a99cec3eec8bfc5a2f68941286a4a3d0fd2371
Mitchell Mohorovich <mitchell@mohorovich.com>
2020-08-28 23:08:24 -0400

    Remove unecessary class, this isn't Java

--- auth.py
+++ auth.py
@@ -1,15 +1,13 @@
 import keyring
 
 
-class Auth:
-    @staticmethod
-    def getCredential(app, username):
-        return keyring.get_credential(app, username)
+def getCredential(app, username):
+    return keyring.get_credential(app, username)
 
-    @staticmethod
-    def getPassword(app, username):
-        return keyring.get_password(app, username)
 
-    @staticmethod
-    def setPassword(app, username, password):
-        keyring.set_password(app, username, password)
+def getPassword(app, username):
+    return keyring.get_password(app, username)
+
+
+def setPassword(app, username, password):
+    keyring.set_password(app, username, password)
