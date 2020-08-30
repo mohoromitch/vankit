7bb9ce6a0af7b29c5c3c3732812c6386e30dfb15
Mitchell Mohorovich <mitchell@mohorovich.com>
2020-08-29 20:35:37 -0400

    Add reddit to constants

--- constants.py
+++ constants.py
@@ -1,11 +1,14 @@
 class AppConstant:
     INSTAGRAM = "instagram"
     TWITTER = "twitter"
+    REDDIT = "reddit"
 
 
 APP_INPUT_MAP = {
     "instagram": AppConstant.INSTAGRAM,
     "i": AppConstant.INSTAGRAM,
     "twitter": AppConstant.TWITTER,
-    "t": AppConstant.TWITTER
+    "t": AppConstant.TWITTER,
+    "reddit": AppConstant.REDDIT,
+    "r": AppConstant.REDDIT
 }
